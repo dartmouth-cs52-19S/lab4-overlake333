f45e89ecce73b991f0fdca518761973a0edcf12d
overlake333 <taylor.m.olson.21@dartmouth.edu>
2019-05-11 23:52:33 -0400

    lab5 pt2

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const ROOT_URL = 'http://localhost:9090/api';
+const ROOT_URL = 'https://overlake333-lab5pt2.herokuapp.com/api';
 // const ROOT_URL = 'https://lab5-overlake333.herokuapp.com/api';
 // const ROOT_URL = 'https://cs52-blog.herokuapp.com/api';
 const API_KEY = '?key=t_olson';
